048d298c8ac61af9ac23906a48729d3617bdc713
Devansh <f20220457@pilani.bits-pilani.ac.in>
2024-05-30 20:59:43 +0530

    made some changes

--- HardHat/hardhat.config.js
+++ HardHat/hardhat.config.js
@@ -2,10 +2,9 @@ require("@nomicfoundation/hardhat-toolbox");
 require("./tasks/blockNumber"); // You need to import the task from its file
 /** @type import('hardhat/config').HardhatUserConfig */
 require("hardhat-gas-reporter")
-const sepoliaURL="https://eth-sepolia.g.alchemy.com/v2/5OQ4Caki1L3ZKlcqHXgZCzO-iS0uZ_Ek";
-const privateKey="b09671918bf15c9e6e0d80080b742bd114c2d01e09c5aaa92ccf714285faeb98";
-// const sepoliaURL=process.env.SEPOLIA_RPC_URL
-// const privateKey=process.env.PRIVATE_KEY
+
+const sepoliaURL=process.env.SEPOLIA_RPC_URL
+const privateKey=process.env.PRIVATE_KEY
 module.exports = {
   solidity: "0.8.7",
   networks:{ // We can create various testnets where we can deploy
@@ -20,7 +19,7 @@ module.exports = {
     }  
   },
   etherscan:{
-    apiKey:"FZZ5XBZQQT6MTA6SX1Z5YR1BNZJR8UCFI4",  // Api key for verification
+    apiKey:process.env.apiKey,  // Api key for verification
   },
   gasReporter:{
     enabled:true, // Tells how much our contract and methods cost 
--- hardhat-fundME/tasks/unit/FundMe.test.js
+++ hardhat-fundME/tasks/unit/FundMe.test.js
@@ -1,10 +1,11 @@
 const {deployments,ethers, getNamedAccounts}=require("hardhat")
-const {assert}=require("chai")
+const {assert,expect}=require("chai")
 
 describe("FundMe", async function(){
     let fundMe;
     let deployer;
     let mockV3Aggregator;
+    const sendValue="1000000000000" // 
     beforeEach(async function(){ 
         //deploying our fund ME contract
         const accounts=await ethers.getSigners(); // to get accounts directly from hardaht config is this method. Returns whatever there is in account section of that particular netwrok
@@ -21,4 +22,72 @@ describe("FundMe", async function(){
 
         })
     })
+    describe("fund",async function(){ //npx harhat test --grep "amount funded"
+        it("Fails if u dont send enough eth",async function(){
+            await expect(fundMe.fund()).to.be.revertedWith("You need to spend more ETH"); // Used expect from chai to expect the failure of things
+        })
+        it("Updates the amount funded ",async function(){
+            await fundMe.fund({value:sendValue})
+            const response=await fundMe.addressToAmountFunded(deployer) // Gets the amount funded by that particular user
+            assert.equal(response.toString(),sendValue.toString());
+        })
+        it("Adds funder to array fo funders",async function(){
+            await fundMe.fund({value:sendValue})
+            const funder=await fundMe.funders(0)
+            assert.equal(funder,deployer)
+        })
+    })
+    describe("Withdraw",async function(){
+        // Before funding we nee to fund some money into the contract
+        beforeEach(async function(){
+            await fundMe.fund({value:sendValue})
+        })
+        it("Withdraw ETH from a single founder",async function(){
+           
+            const transactionResponse=await fundMe.withdraw();
+            const transReceipt=await transactionResponse.wait(1);
+            //But how to get the gas cost
+            const {gasUsed,effectiveGasPrice}=transReceipt
+            const gasCost=gasUsed.mul(effectiveGasPrice)
+            /// 
+            const endingFundMeBalance= await fundMe.provider.getBalance(fundMe.address)
+            const endingDeployerBalance= await fundMe.provider.getBalance(deployer)
+        
+            assert.equal(endingFundMeBalance,0)
+            assert.equal(startingFundMeBalance.add(startingDeployerBalance),endingDeployerBalance.add(gasCost).toString());
+        })
+    })
+    it("allows us to withdraw with multiple accounts",async function(){
+        const accounts = await ethers.getSigners();
+        for(let i=1;i<6;i++){
+            const fundMeConnectedContract=await fundMe.connect(accounts[i]);
+            await fundMeConnectedContract.fund({value:sendValue})
+            
+        }
+        const startingFundMeBalance=await fundMe.provider.getBalance(fundMe.address)  // Could have also used ethers.provider
+        const startingDeployerBalance=await fundMe.provider.getBalance(deployer)
+        const transactionResponse=await fundMe.withdraw();
+        const transReceipt=await transactionResponse.wait(1);
+            //But how to get the gas cost
+        const {gasUsed,effectiveGasPrice}=transReceipt
+        const gasCost=gasUsed.mul(effectiveGasPrice)
+        const endingFundMeBalance= await fundMe.provider.getBalance(fundMe.address)
+        const endingDeployerBalance= await fundMe.provider.getBalance(deployer)
+    
+        assert.equal(endingFundMeBalance,0)
+        assert.equal(startingFundMeBalance.add(startingDeployerBalance),endingDeployerBalance.add(gasCost).toString());
+         // Making sure the funders are reset properly
+         await expect(fundMe.funders(0)).to.be.reverted // Throws error as sabko khali kar diya hai
+
+         for(i=1;i<6;i++){
+            assert.equal(await fundMe.addressToAmountFunded(accounts[i].address),0)
+         }
+         if("only allows the owner to withdraw",async function(){
+            const accounts=await ethers.getSigners();
+            const attacker=accounts[1];
+            const attackerConnectedContract=await fundMe.connect(attacker); //First the 
+            await expect(attackerConnectedContract.withdraw()).to.be.revertedWith("FundMe__NotOwner")
+         })
+    })
+
 })
